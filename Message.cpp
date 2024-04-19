30a259a71f1da26e1b4c91d87e76dab77317f579
David Beattie <C2222071@live.tees.ac.uk>
2024-04-19 11:55:37 +0100

    saving

--- Message.cpp
+++ Message.cpp
@@ -63,6 +63,65 @@ void ReadMsg(Message m, Msg& msg)
     MsgPos* m_pos;
     MsgRand* m_rand;
     MsgSelect* m_select;
+    std::stringstream ss;
+
+    m.first >> msg.msgType;
+    switch(msg.msgType) {
+
+        // Register Message
+        case MsgType::Register:
+        m_reg = new MsgReg();
+        if (m.first >> m_reg->id >> m_reg->username){
+            std::memcpy(&msg, m_reg, sizeof(MsgReg));
+        }
+        else{
+            ss << "message::readMsg Register message, failed!\n";
+        }
+        delete m_reg;
+        break;
+
+        // Select Message
+        case MsgType::Select:
+        m_select = new MsgSelect();
+        if (m.first >> m_select->id >> m_select->builderID){
+            std::memcpy(&msg, m_select, sizeof(MsgSelect));
+        }
+        else{
+            ss << "message::readMsg Select message, failed!\n";
+        }
+        delete m_select;
+        break;
 
 
+        case MsgType::Build:
+        case MsgType::Move:
+
+        // Place Message
+        case MsgType::Place:
+        m_pos = new MsgPos();
+        m_pos->msgType = msg.msgType;
+        if (m.first >> m_pos->id >> m_pos->x >> m_pos->y)
+        {
+            std::memcpy(&msg, m_pos, sizeof(MsgPos));
+        }
+        else
+        {
+            ss << "message::readMsg Move message, failed!\n";
+        }
+        delete m_pos;
+        break;
+
+        // Rand Message
+        case MsgType::Rand:
+        m_rand = new MsgRand();
+        if (m.first >> m_rand->id >> m_rand->r)
+        {
+            std::memcpy(&msg, m_rand, sizeof(MsgRand));
+        }
+        else{
+            ss << "message::readMsg Rand message, failed!\n";
+        }
+        delete m_rand;
+        break;
+    }
 }
